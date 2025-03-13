b7254d7335990f8f85521db45d2ceb2895d5d2f0
Clt689 <kx1302@naver.com>
2025-03-13 15:14:30 +0900

    feat: 3.6) Enum 타입

--- section02/dist/chapter4.js
+++ section02/dist/chapter4.js
@@ -7,12 +7,12 @@ let user = {
     bio: "안녕하세요",
     location: "광진구",
 };
-let user2 = {
-    id: 1,
-    name: "홍길동",
-    nickname: "edrendan",
-    birth: "1999.03.16",
-    bio: "안녕하세요",
-    location: "광진구",
+let countryCodes = {
+    Korea: "ko",
+    UnitedState: "us",
+    UnitedKingdom: "uk",
+};
+let countryNumberAndStringCodes = {
+    Korea: 410,
 };
 export {};
