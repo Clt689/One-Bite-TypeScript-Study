c080ddaa31b951f7ae2654026a7b73ab2d577a68
Clt689 <kx1302@naver.com>
2025-03-13 12:02:37 +0900

    feat: 3.3) 배열과 튜플

--- section02/dist/chapter1.js
+++ section02/dist/chapter1.js
@@ -6,5 +6,20 @@ let num4 = -0.123;
 let num5 = Infinity;
 let num6 = -Infinity;
 let num7 = NaN;
-console.log(num3.toFixed(2));
+// string
+let str1 = "hello";
+let str2 = "hello";
+let str3 = `hello`;
+let str4 = `hello ${num1}`;
+// boolean
+let bool1 = true;
+let bool2 = false;
+// null
+let null1 = null;
+// undefined
+let unde1 = undefined;
+// 리터럴 타입
+let numA = 10;
+let strA = "hello";
+let boolA = true;
 export {};
---
+++ section02/src/chapter2.ts
@@ -0,0 +1,30 @@
+// 배열
+let numArr: number[] = [1, 2, 3];
+
+let strArr: string[] = ["hello", "im", "hanbin"];
+
+// 제네릭 문법
+let boolArr: Array<boolean> = [true, false, true];
+
+// 배열 요소들의 타입이 다양할 경우
+let multiArr: (string | number)[] = [1, "hello"];
+
+// 다차원 배열의 타입을 정의하는 방법
+let doubleArr: number[][] = [
+  [1, 2, 3],
+  [4, 5],
+];
+
+// 튜플
+// 길이와 타입이 고정된 배열
+let tup1: [number, number, string] = [1, 2, "b"];
+
+let tup2: [number, string, boolean] = [1, "6", true];
+
+const users: [string, number][] = [
+  ["이한빈", 1],
+  ["이아무개", 2],
+  ["김아무개", 3],
+  ["박아무개", 4],
+  [5, "최아무개"],
+];
